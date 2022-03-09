40b0bcb8468243da4742ce817a81d39ebfa741d3
chakraa1 <anindyachakrabortty@gmail.com>
2022-03-09 22:43:46 +0530

    Initial version

--- Arrays/2D-Arrays/MatrixTranpose.py
+++ Arrays/2D-Arrays/MatrixTranpose.py
@@ -8,5 +8,14 @@ def matrixTranspose(A):
         for irow in range(rows):
             result[icol][irow]=A[irow][icol]
     return result
+
+def TransposeSquareInplaceUpdate(A):
+    n=len(A)
+    for irow in range(n):
+        # Enforcing upper triangle i.e. cols >= rows
+        for icol in range(irow,n):
+            A[irow][icol],A[icol][irow]=A[icol][irow],A[irow][icol]
+    return A
 A=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(matrixTranspose(A))
+print(TransposeSquareInplaceUpdate(A))

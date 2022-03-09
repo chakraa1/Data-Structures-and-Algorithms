bbc25a61a1f57e5dffe30093cfb95d09f03b8564
chakraa1 <anindyachakrabortty@gmail.com>
2022-03-09 20:59:37 +0530

    Initial version

--- Arrays/2D-Arrays/AntiDiagonals.py
+++ Arrays/2D-Arrays/AntiDiagonals.py
@@ -1,4 +1,34 @@
 def anti_diagonal(A):
-    anti_diagonal=[]
+    rows=len(A)
+    cols=len(A[0])
+    anti_diagonal=[[0]*rows for _ in range(2 * rows - 1)]
+    antiDiagRowIndex = 0
+    for icol in range(cols):
+        r = 0
+        c = icol
+        antiDiagColIndex = 0
+        while(r < rows and c > 0):
+           anti_diagonal[antiDiagRowIndex][antiDiagColIndex] = A[r][c]
+           r += 1
+           c -= 1
+           antiDiagColIndex += 1
+        anti_diagonal[antiDiagRowIndex][antiDiagColIndex] = A[r][c]
+        antiDiagColIndex += 1
+        antiDiagRowIndex += 1
+
+    for iRow in range(1,cols):
+        r = iRow
+        c = cols-1
+        antiDiagColIndex = 0
+        while(r < rows and c > 0):
+           anti_diagonal[antiDiagRowIndex][antiDiagColIndex] = A[r][c]
+           r += 1
+           c -= 1
+           antiDiagColIndex += 1
+        antiDiagRowIndex += 1
+        antiDiagColIndex += 1
 
     return anti_diagonal
+A=[[1,2,3],[4,5,6],[7,8,9]]
+A=[[1,2],[3,4]]
+print(anti_diagonal(A))

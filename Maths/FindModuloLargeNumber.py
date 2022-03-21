5dc18a9199f362c7b6f25b0943319c158741a80e
chakraa1 <anindyachakrabortty@gmail.com>
2022-03-21 23:23:11 +0530

    Initial version

---
+++ Maths/FindModuloLargeNumber.py
@@ -0,0 +1,55 @@
+"""
+====================
+Problem Description
+====================
+You are given a large number in the form of a string A where each character denotes a digit of the number.
+You are also given a number B. You have to find out the value of A % B and return it.
+
+====================
+Problem Constraints
+====================
+1 <= A.length() <= 105
+0 <= Ai <= 9
+1 <= B <= 109
+
+====================
+Input Format
+====================
+The first argument is a string A.
+The second argument is an integer B.
+
+====================
+Output Format
+====================
+Return a single integer denoting the value of A % B.
+
+====================
+Example Input
+====================
+Input 1: A = "143", B = 2
+Input 2: A = "43535321", B = 47
+
+====================
+Example Output
+====================
+Output 1: 1
+Output 2: 20
+
+====================
+Example Explanation
+====================
+Explanation 1: 143 is an odd number so 143 % 2 = 1.
+Explanation 2: 43535321 % 47 = 20
+"""
+
+def findMod(A, B):
+    ans = 0
+    for digit in A:
+        ans = (ans * 10 + int(digit)) % B
+    return ans
+
+A = "143"
+B = 2
+A = "43535321"
+B = 47
+print(findMod(A, B))

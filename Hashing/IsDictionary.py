8ce8c6c63f4bb4d5200b25417ab9618f5fac8997
chakraa1 <anindyachakrabortty@gmail.com>
2022-04-08 11:49:16 +0530

    Initial version

--- Hashing/IsDictionary.py
+++ Hashing/IsDictionary.py
@@ -52,19 +52,38 @@ Example Explanation
 Explanation 1: The order shown in string B is: h < s < i for the given words. So return 1.
 Explanation 2: "none" should be present after "no". Return 0.
 """
-def isSorted(word_1,word_2,rank_map):
-    common_len = min(len(word_1), len(word_1))
-    for j in range(common_len):
-        if word_1[j] != word_2[j]:
-            print("Not equal check ", word_1[j], word_2[j])
-            if rank_map[word_1[j]] < rank_map[word_2[j]]:
-                print("rank check ", word_1[j], word_2[j], rank_map[word_1[j]], rank_map[word_2[j]])
-                return False
-            else:
-                return True
+"""
+from functools import cmp_to_key
+def custom_comparator(s1,s2):
+    for a,b in zip(s1,s2):
+        if rank_map[a] < rank_map[b]:
+            return 1
+        elif rank_map[a] > rank_map[b]:
+            return 0
+        else:
+            continue
 
-    return len(word_1) <= len(word_2)
+    if len(s1) < len(s2):
+        return -1
+    elif len(s1) > len(s2):
+        return 1
+    else:
+        return 0
 
+def IsDictionaryCustomComparatorApproach(A,B):
+    global rank_map
+    rank_map = dict()
+    i = 1
+    for char in B:
+        rank_map[char] = i
+        i += 1
+    A_sorted = sorted(A,key=cmp_to_key(custom_comparator))
+    for i in range(len(A)):
+        if A[i] != A_sorted[i]:
+            return 0
+
+    return 1
+"""
 def IsDictionary(A,B):
     char_rank_map = dict()
     i = 1
@@ -75,13 +94,23 @@ def IsDictionary(A,B):
     n = len(A)
 
     for i in range(1,n):
-        if isSorted(A[i-1],A[i],char_rank_map) == False:
+        word_1 = A[i-1]
+        word_2 = A[i]
+        common_len = min(len(word_1), len(word_2))
+        flag = False
+        for j in range(common_len):
+            if word_1[j] != word_2[j]:
+                # Checking first not matching char
+                if char_rank_map[word_1[j]] > char_rank_map[word_2[j]]:
+                    # Checking Rank of first not matching char
+                    return 0
+                else:
+                    flag = True
+                    break
+        # No mismatch , then we're comparing as per length
+        if flag == False and len(word_1) > len(word_2):
             return 0
-        else:
-            return 1
-
     return 1
-
 
 A = ["hello", "scaler", "interviewbit"]
 
@@ -89,7 +118,10 @@ B = "adhbcfegskjlponmirqtxwuvzy"
 """
 A = ["fine", "none", "no"]
 B = "qwertyuiopasdfghjklzxcvbnm"
+
 """
 
 
 print(IsDictionary(A,B))
+#print(IsDictionaryCustomComparatorApproach(A,B))
+#print(approach3(A,B))

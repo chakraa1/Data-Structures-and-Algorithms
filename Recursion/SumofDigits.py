b62cc2661a78fa82c7db4268a561387f29707efa
chakraa1 <anindyachakrabortty@gmail.com>
2022-04-11 21:10:22 +0530

    Initial version

---
+++ Recursion/SumofDigits.py
@@ -0,0 +1,51 @@
+"""
+======================
+Problem Description
+======================
+Given a number A, we need to find the sum of its digits using recursion.
+
+
+======================
+Problem Constraints
+======================
+1 <= A <= 109
+
+======================
+Input Format
+======================
+The first and only argument is an integer A.
+
+======================
+Output Format
+======================
+Return an integer denoting the sum of digits of the number A.
+
+
+======================
+Example Input
+======================
+Input 1: A = 46
+Input 2: A = 11
+
+======================
+Example Output
+======================
+Output 1: 10
+Output 2: 2
+
+======================
+Example Explanation
+======================
+Explanation 1: Sum of digits of 46 = 4 + 6 = 10
+Explanation 2: Sum of digits of 11 = 1 + 1 = 2
+"""
+import sys
+sys.setrecursionlimit(1000000)
+def SumofDigits(A):
+    if A == 0:
+        return 0
+    Sum = A % 10
+    return Sum + SumofDigits(A // 10)
+A = 46
+A = 11
+print("output",SumofDigits(A))

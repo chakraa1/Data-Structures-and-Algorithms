c7e261567fb5bbdf4cb7f424b43d6be63cbff0ea
chakraa1 <anindyachakrabortty@gmail.com>
2022-04-18 17:59:45 +0530

    Initial version

--- LinkedLists/FlattenNestedListIterator.py
+++ LinkedLists/FlattenNestedListIterator.py
@@ -23,32 +23,42 @@ The values of the integers in the nested list are in the range [-1e6, 1e6].
 """
 
 
-class NestedInteger:
-    def __init__(self, x):
-        """
-        Return true if this NestedInteger holds a single integer, rather than a nested list.
-        """
-        print("__init__")
-    def isInteger(self):
-        """
-        Return the single integer that this NestedInteger holds, if it holds a single integer
-        The result is 1e9 if this NestedInteger holds a nested list
-        """
-        print("isInteger")
+# # This is the interface that allows for creating nested lists.
+# # You should not implement it, or speculate about its implementation
+# class NestedInteger:
+#     def __init__(self, x):
 
-    def getInteger(self):
-        """
-        Return the nested list that this NestedInteger holds, if it holds a nested list
-        The result is an empty list if this NestedInteger holds a single integer
-        """
-        print("getInteger")
-    def getList(self):
-        print("getList")
+#     # Return true if this NestedInteger holds a single integer, rather than a nested list.
+#     def isInteger(self):
+
+#     # Return the single integer that this NestedInteger holds, if it holds a single integer
+#     # The result is 1e9 if this NestedInteger holds a nested list
+#     def getInteger(self):
+
+#     # Return the nested list that this NestedInteger holds, if it holds a nested list
+#     # The result is an empty list if this NestedInteger holds a single integer
+#     def getList(self):
+
 
 class NestedIterator:
     def __init__(self, nestedList):
-        print("NestedIterator --> __init__")
+        self.flatten_list = self.genFlattenList(nestedList)
+        self.count = -1
+
+    def genFlattenList(self, nestedList):
+        flatten_list = []
+
+        for item in nestedList:
+            if item.isInteger():
+                flatten_list.append(item.getInteger())
+            else:
+                flatten_list.extend(self.genFlattenList(item.getList()))
+
+        return flatten_list
+
     def next(self):
-        print("next")
+        self.count += 1
+        return self.flatten_list[self.count]
+
     def hasNext(self):
-        print("hasNext")
+        return self.count < len(self.flatten_list) - 1

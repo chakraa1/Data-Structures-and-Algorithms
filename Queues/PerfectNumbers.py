41a6d937d31c2cce3f9db5d43962fd9c8178ae32
chakraa1 <anindyachakrabortty@gmail.com>
2022-04-22 17:55:35 +0530

    Initial version

--- Queues/PerfectNumbers.py
+++ Queues/PerfectNumbers.py
@@ -60,11 +60,86 @@ First four perfect numbers are:
 4. 1221
 
 """
+class LinkedListNode:
+    def __init__(self,x):
+        self.data = x
+        self.next = None
+
+
+class LinkedListNode:
+    def __init__(self,x):
+        self.data = x
+        self.next = None
+
+
+class QueueLL:
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+
+    def Enqueue(self,x):
+        new_node = LinkedListNode(x)
+        if self.head == None:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+        self.size += 1
+
+    def Dequeue(self):
+        if self.head != None:
+            dq_element = self.head.data
+            self.head = self.head.next
+            self.size -= 1
+            return dq_element
+        else:
+            self.tail = None
+            self.size = 0
+
+        return None
+
+    def print_queue(self):
+        itr = self.head
+        while itr != None:
+            print(itr.data,end =" ")
+            itr = itr.next
+        print()
 def PerfectNumbers(A):
-    ans = 0
+    q = QueueLL()
+
+    q.Enqueue(1)
+    q.Enqueue(2)
+
+    cnt = 1
+
+    while True:
+       """
+       Step 1 - Pop from left 
+       """
+       x = str(q.Dequeue())
+
+       """
+       Step 2 - create answer 
+       """
+       ans = x + x[::-1]
+       if cnt == A:
+           break
+
+       """
+        Step 3 - create answer 
+       """
+       q.Enqueue(int(x + '1'))
+       q.Enqueue(int(x + '2'))
+
+       cnt += 1
+
 
     return ans
 
-A = 2
-A = 3
+A = 5
+
 print(PerfectNumbers(A))

1cf8fc27366f170930d3aceff7663d614b72142f
Anindya Chakraborty <46349688+chakraa1@users.noreply.github.com>
2023-06-03 08:30:32 +0530

    Initial version

---
+++ Arrays/TwoPointers/PairDifference.py
@@ -0,0 +1,28 @@
+"""
+Find if pair difference = K .
+    A[j] - A[i] = K (Given)
+Given N distinct elements in a sorted array
+Difficulty Level : Hard
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+
+def check_pair_difference(A,K):
+    n = len(A)
+    p1 = 0
+    p2 = 1
+
+    while p2 < n-1:
+        if A[p2] - A[p1] == K:
+            return True
+        elif A[p2] - A[p1] > K:
+            p1 += 1
+        else:
+            p2 += 1
+
+    return False
+A1 = [-3,0,1,3,6,8,11,14,18,25]
+K1 = 5
+print("Input" , A1,"K ",K1)
+print(check_pair_difference(A1,K1))
+
--- Arrays/TwoPointers/PairSum.py
+++ Arrays/TwoPointers/PairSum.py
@@ -3,9 +3,11 @@ Find if pair sum = K .
     A[i] + A[j] = K (Given)
 Given N distinct elements in a sorted array
 Difficulty Level : Easy
+Time complexity: O(n)
+Space complexity: O(1)
 """
 
-def check_pair_sorted(A,K):
+def check_pair_sum(A,K):
     n = len(A)
     p1 = 0
     p2 = n-1
@@ -24,6 +26,6 @@ K1 = 15
 A2 =[2,5,8,11,15]
 K2 = 16
 print("Input" , A1,"K ",K1)
-print(check_pair_sorted(A1,K1))
+print(check_pair_sum(A1,K1))
 print("Input" , A2,"K ",K2)
-print(check_pair_sorted(A2,K2))
+print(check_pair_sum(A2,K2))

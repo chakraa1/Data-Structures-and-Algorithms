c6ff4175653479d76878dbcc65236fd083ac9530
Anindya Chakraborty <46349688+chakraa1@users.noreply.github.com>
2023-06-04 12:34:14 +0530

    Initial version

--- Recursion/PowFunction.py
+++ Recursion/PowFunction.py
@@ -6,6 +6,14 @@ def calPow(a,n,p):
     else:
         return (a%p*(calPow(a,n//2,p)*calPow(a,n//2,p)%p))
 
+def calPowOptimal(a,n,p):
+    if a == 1 or n == 0:
+        return 1
+    hp = calPowOptimal(a,n//2,p)
+    if n % 2 == 0:
+        return (hp*hp)%p
+    else:
+        return (((hp*hp)%p)*a%p)%p
 a = 2
 n =3
 p = 3
